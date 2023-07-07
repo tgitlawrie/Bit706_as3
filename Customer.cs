34cf5ac75ca5aafb56aaa88884c465afd0a52c81
tgitlawrie <tlawrie@orcon.net.nz>
2023-07-08 10:13:33 +1200

    Add project files.

---
+++ Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bit706_as2
+{
+    public class Customer
+    {
+        private int nextID = 1;
+        private int iD;
+        private string firstName;
+        private string lastName;
+
+        public Customer() {
+            iD = nextID;
+            nextID++;
+        }
+
+        public Customer(string firstName, string lastName) :this() {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        //returns the customers information
+        public string CustomerInfo()
+        {
+            return iD.ToString() + " | " + firstName + " " + lastName;
+        }
+    }
+}
---
+++ CustomerController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bit706_as2
+{
+    class CustomerController
+    {
+        private string errorMessage;
+
+        public string ErrorMessage { get => errorMessage; set => errorMessage = value; }
+
+        private List<Customer> customerList = new List<Customer>();
+
+        public bool AddCustomer(string firstName, string lastName)
+        {
+            //do the things
+            return false;
+        }
+
+        public bool FindCustomerByName(string name)
+        {
+            return false;
+        }
+
+        public bool EditCustomer(string firstName, string lastName)
+        {
+            return false;
+        }
+
+        public bool DeleteCustomer(string firstName, string lastName)
+        {
+            return false;
+        }
+    }
+}

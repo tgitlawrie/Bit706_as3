61c8331c36fc9b9c742a4ef153c010d236363ad6
tgitlawrie <tlawrie@orcon.net.nz>
2023-07-09 13:55:41 +1200

    added more name validations

--- Customer.cs
+++ Customer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Bit706_as2
@@ -15,12 +16,23 @@ namespace Bit706_as2
 
         public Customer(string firstName, string lastName)
         {
+            int nameMaxLength = 20;
+            bool containsOnlyLetters = Regex.IsMatch(firstName + lastName, @"^[a-zA-Z]+$");
             firstName = firstName.Trim().ToLower();
             lastName = lastName.Trim().ToLower();
 
+            //validation guard clauses
             if (firstName == "") { throw new Exception("You must enter a first name!"); }
-
             if (lastName == "") { throw new Exception("You must enter a last name!"); }
+            if (firstName.Length > nameMaxLength)
+            {
+                throw new Exception($"First name must be less than {nameMaxLength} characters long");
+            }
+            if (lastName.Length > nameMaxLength)
+            {
+                throw new Exception($"Last name must be less than {nameMaxLength} characters long");
+            }
+            if (!containsOnlyLetters) { throw new Exception("Customer name must not have special characters or numbers"); }
 
             //id generation placed here instead of using overloaded constructors
             //to ensure id numbers are only assigned to valid customers
